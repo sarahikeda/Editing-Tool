375ac17c36a82d21268bd2858981e08d9b3ff30f
Sarah Ing <sarahikeda@gmail.com>
2018-01-16 13:15:47 -0500

    Add Citi logo and today's date

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import CitiLogo from '../../images/citilogo.png'
 
 class Summary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: ''
+    }
+  }
+
+  getCurrentDate = () => {
+    let today = new Date().toString();
+    return today.split(" ").slice(0,4).join(" ");
+  }
+
+  componentDidMount() {
+    this.setState({
+      currentDate: this.getCurrentDate()
+    })
+  }
+
   render() {
     return (
       <div className="summary">
-        <p><span className="section-title">Summary </span><span className="section-explanation">(For display in velocity and digest emails)</span></p>
-        <textarea className="summary-text" placeholder="Type your summary here..."></textarea>
+        <img className="citi-logo" src={CitiLogo} alt="citi-logo" />
+        <p className="current-date">{this.state.currentDate}</p>
       </div>
     );
   }
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 class Title extends Component {
   render() {
     return (
-      <div className="summary">
-        <p><span className="section-title">Title</span></p>
-        <p className="date">Tuesday, November 28, 2017 9:00 AM</p>
+      <div className="title">
+        <p className="section-title">Quick Links:</p>
       </div>
     );
   }
