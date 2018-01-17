34040384d69ff613041c8ac101c7b358c743c1e4
Sarah Ing <sarahikeda@gmail.com>
2018-01-17 15:48:37 -0500

    Add separate component for CDK editor which shows different icons depending on events

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import CKEditor from "react-ckeditor-component";
+import PlusIcon from '../PlusIcon/PlusIcon';
+
+class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'Drag files here or just start typing',
+      showNewEditor: false,
+      icons: 'toolbar',
+    }
+  }
+
+  updateContent = (newContent) => {
+    this.setState({
+      content: newContent,
+    })
+  }
+
+  onChange = (event) => {
+    console.log("onChange fired with event info: ", event);
+    var newContent = event.editor.getData();
+    event.editor.destroy(true);
+    this.setState({
+      content: newContent,
+      showNewEditor: !this.state.showNewEditor,
+      icons: 'clipboard, pastetext, pastefromword,specialchar,tab,table,tabletools,undo,wsc,a11yhelp,about,basicstyles,sourcearea,showborders,scayt,resize,maximize,link,blockquote,format,horizontalrule,stylescombo,list,indent,indentlist,removeformat'
+    })
+  }
+
+  onBlur = (event) => {
+    console.log("onBlur event called with event info: ", event);
+  }
+
+  afterPaste = (event) => {
+    console.log("afterPaste event called with event info: ", event);
+  }
+
+  render() {
+    return (
+      <div className="editor">
+        <PlusIcon handleClick={this.onClick}/>
+        {this.state.showNewEditor &&
+          <CKEditor
+            config={
+              {
+                removePlugins: this.state.icons
+              }
+            }
+          />
+        }
+        <CKEditor
+          activeClass="p10"
+          content={this.state.content}
+          config={
+            {
+              removePlugins: 'toolbar'
+            }
+          }
+          events={{
+            "blur": this.onBlur,
+            "afterPaste": this.afterPaste,
+            "change": this.onChange
+          }}
+         />
+      </div>
+    )
+  }
+}
+
+export default Editor;
