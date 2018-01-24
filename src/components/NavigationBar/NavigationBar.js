614e7e6820c73a912bf96d82e489d6f03b86d224
Sarah Ing <sarahikeda@gmail.com>
2018-01-24 11:21:25 -0500

    Remove extra elements from Navigation

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -13,22 +13,7 @@ class NavigationBar extends Component {
          </Navbar.Brand>
        </Navbar.Header>
        <Nav>
-        <NavItem eventKey={1} href="#">Save as Template</NavItem>
-        <NavItem eventKey={1} href="#" className="delete-link">Delete</NavItem>
-        <NavDropdown eventKey={3} title="Template" id="navigation-dropdown">
-          <div className="row template-articles">
-            <RecentDocs/>
-          </div>
-        </NavDropdown>
-        <NavDropdown eventKey={3} title="Drafts" id="draft-options">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-        </NavDropdown>
-        <NavItem eventKey={1} href="#">Content</NavItem>
-        <Button className="publish-button">
-          <span className="add-user-icon"><FontAwesome name='user-plus'/></span>
-          Publish
-        </Button>
+        <NavItem eventKey={1} href="#" className="delete-link">Global Technology Daily</NavItem>
       </Nav>
     </Navbar>
     );
