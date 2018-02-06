7238e565097f1272560d64b9a654e330854929c1
Saumyajit Chowdhury <saumyajit.c@gmail.com>
2018-02-06 02:09:39 -0500

    IMplemented popover functionality one at a time

--- src/components/ResearchContent/PostNavs/PostNavs.js
+++ src/components/ResearchContent/PostNavs/PostNavs.js
@@ -19,7 +19,7 @@ class PostNav extends Component {
         return (
             <div className="float-right">
                 <FontAwesomeIcon id={"pop" + this.props.id} icon="times-circle" transform="rotate-45" onClick={() => this.toggle()}/>
-                <Popover placement="bottom" isOpen={this.state.popoverOpen} target={"pop" + this.props.id} toggle={this.toggle}>
+                <Popover placement="bottom" isOpen={this.state.popoverOpen} target={"pop" + this.props.id} toggle={() => this.toggle()}>
                     <PopoverHeader>Popover Title</PopoverHeader>
                 </Popover>
             </div>
