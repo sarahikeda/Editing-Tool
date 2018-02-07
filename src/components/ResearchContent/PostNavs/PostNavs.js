df7f6bfc47b6bb252b90da6ca97a3309f5ffdd6f
Saumyajit Chowdhury <saumyajit.c@gmail.com>
2018-02-07 00:07:00 -0500

    changed method signature to recognize ES6 patterm

--- src/components/ResearchContent/PostNavs/PostNavs.js
+++ src/components/ResearchContent/PostNavs/PostNavs.js
@@ -12,19 +12,20 @@ class PostNav extends Component {
         };
       }
     
-    toggle() {
+    toggle = () => {
         this.setState({
             popoverOpen: !this.state.popoverOpen
         });
     }
+    
     render() {
         return (
             <div className="float-right">
-                <FontAwesomeIcon id={`pop${this.props.id}`} icon="times-circle" size="lg" transform="rotate-45" onClick={() => this.toggle()}/>
-                <Popover placement="bottom" isOpen={this.state.popoverOpen} target={`pop${this.props.id}`} toggle={() => this.toggle()}>
+                <FontAwesomeIcon id={`pop${this.props.id}`} icon="times-circle" size="lg" transform="rotate-45" onClick={this.toggle}/>
+                <Popover placement="bottom" isOpen={this.state.popoverOpen} target={`pop${this.props.id}`} toggle={this.toggle}>
                     <PopoverHeader>
-                        <button className="mr-1 btn btn-xs" onClick={() => this.toggle()}><Icon icon={ICONS.BLURB} size={14} color="#2f63af"/></button>
-                        <button className="btn btn-xs" onClick={() => this.toggle()}><Icon icon={ICONS.BLURB} size={14} color="#2f63af"/></button>
+                        <button className="mr-1 btn btn-xs" onClick={this.toggle}><Icon icon={ICONS.BLURB} size={14} color="#2f63af"/></button>
+                        <button className="btn btn-xs" onClick={this.toggle}><Icon icon={ICONS.BLURB} size={14} color="#2f63af"/></button>
                     </PopoverHeader>
                 </Popover>
             </div>
--- src/components/ResearchContent/ResearchContent.js
+++ src/components/ResearchContent/ResearchContent.js
@@ -4,7 +4,8 @@ import Post from './ResearchPosts/ResearchPosts';
 
 class ResearchContent extends Component {
   state = {
-    posts: []
+    posts: [],
+    activeTab: '1'
   }
 
   componentDidMount() {
@@ -22,6 +23,14 @@ class ResearchContent extends Component {
     }
   }
 
+  toggleTab = tab => {
+    let tabState = this.state.activeTab;
+    if(this.state.activeTab !== tab){
+        tabState = tab;
+        this.setState({"activeTab": tabState});
+    }
+  }
+
   render() {
     const posts = this.state.posts.map(post => {
       return <Post key={post.id} title={post.title} body={post.body} author={post.author} date={post.date} id={post.id}/>;
@@ -31,9 +40,9 @@ class ResearchContent extends Component {
         <h5 className="research-head-title">- Research Content</h5>
 
         <ul className="nav nav-tabs mb-2">
-              <li className="nav-item"><a className="nav-link active">Research Feed</a></li>
-              <li className="nav-item"><a className="nav-link">My Previuos Content</a></li>
-              <li className="nav-item"><a className="nav-link">Following</a></li>
+              <li className="nav-item"><a className="nav-link active" onClick={this.toggleTab.bind(this, "1")}>Research Feed</a></li>
+              <li className="nav-item"><a className="nav-link" onClick={this.toggleTab.bind(this, "2")}>My Previous Content</a></li>
+              <li className="nav-item"><a className="nav-link" onClick={this.toggleTab.bind(this, "3")}>Following</a></li>
           </ul>
 
           <div className="input-group stylish-input-group mb-3">
