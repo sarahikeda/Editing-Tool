f7bc77e5134e3ce688c96c7a29d8a57cf8c9d37d
Sarah Ing <sarahikeda@gmail.com>
2018-02-08 13:56:42 -0500

    Add component to show html text in a modal from the ck editor

---
+++ src/components/ContentModal/ContentModal.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+export default class ContentModal extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+    render() {
+      return (
+        <Modal isOpen={this.props.modal} toggle={this.props.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.props.toggle}>HTML from Editor</ModalHeader>
+          <ModalBody>
+            {this.props.editorContent}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={this.props.toggle}>Close</Button>
+          </ModalFooter>
+        </Modal>
+      );
+    }
+}
